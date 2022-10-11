e6871af615e7f2dd116bef02cc763f2d997cb8b5
Chintan <patelcd797@gmail.com>
2022-10-11 09:31:27 +0530

    chore: added users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_060838) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_035126) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
