47784bcb76426cfb1ddc30b3d72aca658c07d6ef
Chintan <patelcd797@gmail.com>
2022-10-11 12:12:13 +0530

    use profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def show
+        @user = User.find(params[:id])
+        @articles =  @user.articles
+    end
+
     def create
        
         @user = User.new(user_param)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def gravetar_for(user, options = { size: 80 })
+        email_Address = user.email.downcase
+        hase = Digest::MD5.hexdigest(email_Address)
+        size = options.size
+        url = "https://www.gravatar.com/avatar/#{hase}?s=200"
+        image_tag(url, alt: user.name, class: "rounded shadow mx-auto d-block")
+    end
 end
